bfdbe0982fd2ab9299e693956307543025b62c1d
Bruno Thienel <BrunoTh@users.noreply.github.com>
2021-03-17 06:40:01 +0000

    documentation

--- web/plugins/userdb/telegram_user_attribute.py
+++ web/plugins/userdb/telegram_user_attribute.py
@@ -3,20 +3,26 @@
 from cmk.gui.plugins.userdb import UserAttribute, user_attribute_registry
 from cmk.gui.valuespec import TextUnicode
 
+# This adds a new attribute to the User objects.
 @user_attribute_registry.register
 class TelegramUsernameUserAttribute(UserAttribute):
     @classmethod
     def name(cls):
+        # The internal name. This also affects the name of the env variable (see add_custom_macro()).
         return "telegram_username"
     
     def topic(self):
+        # Show the field under "Identity" in the user edit form.
         return "ident"
     
     def valuespec(self):
+        # The type and title of the field.
         return TextUnicode(title="Telegram Username")
 
     def show_in_table(self):
+        # Also add a new column to the user list.
         return True
 
     def add_custom_macro(self):
+        # Make this variable available in notifications. It's passed as environment variable NOTIFY_CONTACT_TELEGRAM_USERNAME.
         return True
--- web/plugins/wato/notify_telegram_gui.py
+++ web/plugins/wato/notify_telegram_gui.py
@@ -4,20 +4,26 @@ from cmk.gui.plugins.wato import notification_parameter_registry, NotificationPa
 # TODO: Also use password store.
 from cmk.gui.valuespec import Dictionary, TextUnicode
 
-
+# This adds a new gui component for the notification method.
 @notification_parameter_registry.register
 class NotificationParameterTelegram(NotificationParameter):
     @property
     def ident(self):
+        # The name of the notification plugin in `~/local/share/check_mk/notifications/`.
+        # If this does not match with the script name, the generic gui component for custom plugins is used.
         return "telegram.py"
 
     def _get_config_elements(self):
+        # Add the text field with a title and a help text. The internal name is `bottoken`. This name is also used for the env variable that
+        # is set when the notification plugin itself (telegram.py) gets called.
+        # The name of this env variable is NOTIFY_PARAMETER_BOTTOKEN.
         return [
             ("bottoken", TextUnicode(title="Bot Token", allow_empty=False, help="The token you got from BotFather after the Bot creation.")),
         ]
 
     @property
     def spec(self):
+        # The gui component itself. This will create a dictionary (internal datatype) with the elements from _get_config_elememts().
         return Dictionary(
             title=_("Create notification with the following parameters"),
             help="The notifcations are sent to the users in the corresponding notification group. Each user needs the attribute 'Telegram Username'. Set it for every user in 'Setup > Users'.",
